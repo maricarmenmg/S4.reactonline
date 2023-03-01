50eeef04e1fb0084d85790e3b56205da1a8897f5
maricarmen.martin <maricarmenmartin@iMac-de-Maria.local>
2023-03-01 19:34:24 +0100

    Nivel 1 - Ejercicio 4

--- src/films.js
+++ src/films.js
@@ -8,46 +8,29 @@ function getAllDirectors(array) {
   return allDirectors;
 }
 
-
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
   const moviesFromDirector = array.filter(movies => movies.director === director);
   console.log("EXERCICE 2 ->", moviesFromDirector);
   return moviesFromDirector;
-
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-
   const moviesOfDirector = array.filter(movies => movies.director === director);
   const averageMoviesOfDirector = moviesOfDirector.reduce((acc, movies) => acc + movies.score, 0) ;
   console.log("EXERCICE 3 ->", averageMoviesOfDirector);
   return averageMoviesOfDirector / moviesOfDirector.length.toFixed(2);
-
 }
 
-// Exercise 3: Calculate the average of the films of a given director.
-/*function moviesAverageOfDirector(array, director) {
-  const moviesFromDirector = array.filter((film) => film.director === director);
-  const averageScore = moviesFromDirector.reduce((acc, cur) => acc + cur.score, 0)
-  console.log("EXERCICE 3 ->", averageScore);
-  return averageScore / moviesFromDirector.length.toFixed(2)
-}*
-
-
-function moviesAverageOfDirector(movies, targetDirector) {
-  const filteredMovies = movies.filter(({ director }) => director === targetDirector);
-  const sum = filteredMovies.reduce((acc, { rating }) => acc + rating, 0);
-  return filteredMovies.length ? sum / filteredMovies.length : 0;
-}*/
-
-
-
-// Exercise 4:  Alphabetic order by title 
+// Exercise 4:  Alphabetic order by title
 function orderAlphabetically(array) {
-  
+  const orderBytitle = array.map(movie => movie.title);
+  const firstTwentyMovies = orderBytitle.sort().slice(0, 20);
+  console.log("EXERCICE 4 ->", firstTwentyMovies);
+  return firstTwentyMovies;
 }
+
 
 // Exercise 5: Order by year, ascending
 function orderByYear() {
