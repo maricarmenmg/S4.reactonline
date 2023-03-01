b7d4fbe8fb7bcb1fd72264c88aa6408f539022b3
maricarmen.martin <maricarmenmartin@iMac-de-Maria.local>
2023-03-01 16:49:26 +0100

    Nivel 1 - Ejercicio 3

--- src/films.js
+++ src/films.js
@@ -4,7 +4,7 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
   const allDirectors = array.map(movies => movies.director);
-  console.log("EXERCICE 1 ALL DIRECTORS->", allDirectors);
+  console.log("EXERCICE 1 ->", allDirectors);
   return allDirectors;
 }
 
@@ -12,15 +12,37 @@ function getAllDirectors(array) {
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
   const moviesFromDirector = array.filter(movies => movies.director === director);
-  console.log("EXERCICE 2  DIRECTOR->", moviesFromDirector);
+  console.log("EXERCICE 2 ->", moviesFromDirector);
   return moviesFromDirector;
 
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+
+  const moviesOfDirector = array.filter(movies => movies.director === director);
+  const averageMoviesOfDirector = moviesOfDirector.reduce((acc, movies) => acc + movies.score, 0) ;
+  console.log("EXERCICE 3 ->", averageMoviesOfDirector);
+  return averageMoviesOfDirector / moviesOfDirector.length.toFixed(2);
+
 }
+
+// Exercise 3: Calculate the average of the films of a given director.
+/*function moviesAverageOfDirector(array, director) {
+  const moviesFromDirector = array.filter((film) => film.director === director);
+  const averageScore = moviesFromDirector.reduce((acc, cur) => acc + cur.score, 0)
+  console.log("EXERCICE 3 ->", averageScore);
+  return averageScore / moviesFromDirector.length.toFixed(2)
+}*
+
+
+function moviesAverageOfDirector(movies, targetDirector) {
+  const filteredMovies = movies.filter(({ director }) => director === targetDirector);
+  const sum = filteredMovies.reduce((acc, { rating }) => acc + rating, 0);
+  return filteredMovies.length ? sum / filteredMovies.length : 0;
+}*/
+
+
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
