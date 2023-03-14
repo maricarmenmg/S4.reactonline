e7af7c91de8d4f77ed6146ada67c935dd543e693
maricarmen.martin <maricarmenmartin@iMac-de-Maria.local>
2023-03-14 15:52:33 +0100

    Ejercicio 2

--- src/films.js
+++ src/films.js
@@ -30,84 +30,36 @@ function getMoviesFromDirector(array, director) {
   return moviesFromDirector;
 }
 
-
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  const moviesOfDirector = array.filter(movies => movies.director === director); // filter the array to get the movies of the director  
-  const averageMoviesOfDirector = moviesOfDirector.reduce((acc, movies) => acc + movies.score, 0) ; // calculate the average of the movies of the director
-  //console.log("EXERCICE 3 ->", averageMoviesOfDirector);
-  return averageMoviesOfDirector / moviesOfDirector.length.toFixed(2);  // calculate the average of the movies of the director
+  
 }
 
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  const orderByTitle = array.map(movie => movie.title); // create a copy of the array
-  const firstTwentyMovies = orderByTitle.sort().slice(0, 20); // slice the array to get the first 20 elements
-  //console.log("EXERCICE 4 ->", firstTwentyMovies);
-  return firstTwentyMovies;
+  
 }
-
 
 // Exercise 5: Order by year, ascending
-function orderByYear(array) {
-  const orderYear = [...array].sort((a, b) => { return (a.year - b.year) || (a.title.localeCompare(b.title))})  // sort the array by year
-  //console.log("EXERCICE 5 ->", orderYear);
-  return orderYear;
-}
+function orderByYear() {
 
+}
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory(array, category) {
-  const averageByCat = array.filter(element => {  // filter the array to get the movies of the category
-         if (typeof(element.score) !== "number"); // check if the element is a movie
-              else return element.genre.includes(category);   
-  });
+function moviesAverageByCategory() {
 
-  const average = Number((averageByCat.reduce((acc, cur) => acc + cur.score, 0) / averageByCat.length).toFixed(2)); // calculate the average of the movies of the category
-  //console.log("EXERCISE 6 ->", average);
-  return average;
 }
 
-
-// Nivell 2 ★★ ⤵ 
-
 // Exercise 7: Modify the duration of movies to minutes
+function hoursToMinutes() {
 
-function hoursToMinutes(movies) {
-  return movies.map(movie => {
-    const newMovie = { ...movie }; // create a copy of the movie object
-    const durationArray = newMovie.duration.split(' '); // split the duration string by spaces
-    let minutes = 0;
-     for (let i = 0; i < durationArray.length; i++) {
-       const durationPart = durationArray[i];
-          if (durationPart.includes('h')) { // duration part is hours
-        minutes += parseInt(durationPart) * 60; // multiply the hours by 60 to get the minutes
-          } else if (durationPart.includes('min')) { // duration part is minutes
-        minutes += parseInt(durationPart); // add the minutes to the minutes  
-      }
-    }
-    newMovie.duration = minutes; // replace duration string with duration in minutes
-    //console.log("EXERCISE 7 ->", newMovie);
-    return newMovie;
-  });
 }
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear(array, year) {
-
-  const newBestFilm = [...array]; // create a copy of the array
-  const moviesOfYear = newBestFilm.filter((movie) => movie.year === year);  // filter the array to get the movies of the year
-  const bestFilmOfYear = moviesOfYear.reduce((best, current) => best.score >= current.score ? best : current);  // get the best film of the year
-  const highestScoreOfYear = bestFilmOfYear.score;  
-  const bestMovieOfYear = moviesOfYear.filter((movie) => movie.score === highestScoreOfYear    
-);
+function bestFilmOfYear() {
   
-  //console.log("EXERCICE 8 ->", bestMovieOfYear);
-  return bestMovieOfYear;
 }
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -123,4 +75,5 @@ if (typeof module !== 'undefined') {
     bestFilmOfYear,
   };
 }
+
 
